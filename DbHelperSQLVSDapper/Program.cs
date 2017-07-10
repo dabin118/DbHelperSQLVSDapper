0c15ce1f33b7707de3560ba63603eb778319390b
huangwenbin <huangwenbin@mistong.com>
2017-07-10 10:47:45 +0800

    add 创建项目

---
+++ DbHelperSQLVSDapper/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections;
+namespace DbHelperSQLVSDapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //在tempDB库中新建表Customer
+            SqlHelper.EnsureDbSetup();
+            Console.WriteLine("DbHelperSQL和Dapper性能对比开始......");
+            DbHelperSQLTest();
+            Console.WriteLine();
+            Console.WriteLine();
+            DapperTest();
+            Console.WriteLine("按回车键退出");
+            Console.ReadKey();
+        }
+        static  void DbHelperSQLTest()
+        {
+            Hashtable ht = new Hashtable();
+            var sw = new Stopwatch();
+            sw.Start();
+            //插它五百次
+            for (int i = 0; i < 500;i++ )
+            {
+                SqlParameter[] param = { new SqlParameter ("@CustomerId",SqlDbType.NChar,5),
+                                     new SqlParameter("@companyName",SqlDbType.NVarChar,40)
+                                                                         
+                                     };
+                param[0].Value = SqlHelper.TestId;
+                param[1].Value = SqlHelper.InsertCompanyName;
+             
+                for (int j = 0; j < i;j++ )
+                {
+                    SqlHelper.CreateStatement += ";";
+                }
+                ht.Add(SqlHelper.CreateStatement, param);
+            }
+           //利用事务批量执行Sql插入操作
+            DbHelperSQL.connectionString = SqlHelper.ConnectionString;
+            DbHelperSQL.ExecuteSqlTran(ht);
+            sw.Stop ();
+            Console.WriteLine("DbHelperSQL执行事务插入500条记录耗时(毫秒):  "+sw.ElapsedMilliseconds);
+            //测试DbHelperSQL Select top 500耗费时间
+            sw.Reset();
+            sw.Start();
+            DbHelperSQL.Query(SqlHelper.ReadStatement);
+          
+            sw.Stop();  
+            Console.WriteLine("DbHelperSQL选择500条记录耗时(毫秒):  " + sw.ElapsedMilliseconds);
+        }
+        static void DapperTest()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            List<Customer> customers = new List<Customer>();
+            for (int i = 0; i < 500; i++)
+            {
+
+                customers.Add(new Customer { CustomerId = SqlHelper.TestId, CompanyName = SqlHelper.InsertCompanyName });
+
+            }
+          SqlMapperUtil.InsertMultiple<Customer>(SqlHelper.CreateStatement, customers, SqlHelper.ConnectionString);
+      
+            sw.Stop();
+            Console.WriteLine("Dapper插入500条记录耗时(毫秒):  " + sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            //测试Dapper进行 Select top 500耗费时间
+            sw.Start();
+           SqlMapperUtil.SqlWithParams<Customer>(SqlHelper.ReadStatement, null,SqlHelper.ConnectionString);
+           
+            sw.Stop(); 
+            Console.WriteLine("Dapper选取前500条记录耗时(毫秒):  " + sw.ElapsedMilliseconds);
+        }
+    }
+}
+
---
+++ DbHelperSQLVSDapper/SqlHelper.cs
@@ -0,0 +1,61 @@
+﻿using System.Data.SqlClient;
+
+namespace DbHelperSQLVSDapper
+{
+    internal static class SqlHelper
+    {
+        public static readonly string ConnectionString =
+            @"Data Source=.\sqlexpress;Initial Catalog=tempdb;Integrated Security=True;uid=sa;pwd=123456";
+
+        public static  string CreateStatement =
+            @"INSERT dbo.Customers (CustomerID, CompanyName) SELECT @CustomerId, @companyName";
+
+      //  public static readonly string ReadStatement = @"SELECT * FROM dbo.Customers WHERE CustomerId = @CustomerId";
+public static readonly string ReadStatement = @"SELECT top 500  * FROM dbo.Customers ";
+        public static readonly string UpdateStatement =
+            @"UPDATE dbo.Customers SET CompanyName = @companyName WHERE CustomerId = @CustomerId";
+
+        public static readonly string DeleteStatement = @"DELETE FROM dbo.Customers WHERE CustomerId = @CustomerId";
+
+       
+
+
+        public static readonly string TestId = "TEST";
+        public static readonly string InsertCompanyName = "Inserted company";
+        public static readonly string UpdateCompanyName = "Updated company";
+
+        internal static void EnsureDbSetup()
+        {
+            using (var cnn = GetOpenConnection())
+            {
+                var cmd = cnn.CreateCommand();
+                cmd.CommandText = @"
+    if (OBJECT_ID('Customers') is null)
+    begin
+    CREATE TABLE [dbo].[Customers](
+	    [CustomerID] [nchar](5) NOT NULL,
+	    [CompanyName] [nvarchar](40) NOT NULL,
+	    [ContactName] [nvarchar](30) NULL,
+	    [ContactTitle] [nvarchar](30) NULL,
+	    [Address] [nvarchar](60) NULL,
+	    [City] [nvarchar](15) NULL,
+	    [Region] [nvarchar](15) NULL,
+	    [PostalCode] [nvarchar](10) NULL,
+	    [Country] [nvarchar](15) NULL,
+	    [Phone] [nvarchar](24) NULL,
+	    [Fax] [nvarchar](24) NULL)
+    end
+    ";
+                cmd.Connection = cnn;
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static SqlConnection GetOpenConnection()
+        {
+            var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
